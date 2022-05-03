a70e27c959915644c33a8f1802c09559dd15aec6
Florent <florent.hagard@gmail.com>
2022-05-03 11:40:27 +0200

    auto commit

--- main.go
+++ main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"fmt"
 
+	"github.com/ief2i-florent/golang/internal/fssync"
 	"github.com/spf13/cobra"
 )
 
 func main() {
 	var root = &cobra.Command{
-		Use:   "app [source] [destination]",
+		Use:   "app [source] [target]",
 		Short: "Permet de copier des fichiers",
 		Args:  cobra.ExactArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			source := args[0]
-			destination := args[1]
+			target := args[1]
 
-			fmt.Println(source, destination)
+			if fssync.FolderExist(source) {
+				fssync.CreateFolderIfNotExist(target)
+				for _, file := range fssync.Scan(source, source) {
+					fssync.Copy(source+file, target+file)
+				}
+			} else {
+				fmt.Println("Source folder not exist")
+			}
 
 		},
 		DisableFlagsInUseLine: true,
